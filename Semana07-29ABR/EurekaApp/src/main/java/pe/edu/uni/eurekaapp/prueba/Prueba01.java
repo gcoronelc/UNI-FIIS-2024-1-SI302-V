9355486051dda2be2e709fa74a1c4bc011ffe311
Gustavo Coronel <gcoronelc@gmail.com>
2024-04-29 09:25:28 -0500

    Semana 7

---
+++ Semana07-29ABR/EurekaApp/src/main/java/pe/edu/uni/eurekaapp/prueba/Prueba01.java
@@ -0,0 +1,21 @@
+package pe.edu.uni.eurekaapp.prueba;
+
+import java.sql.Connection;
+import pe.edu.uni.eurekaapp.db.AccesoDB;
+
+
+public class Prueba01 {
+	
+	public static void main(String[] args) {
+		
+		try {
+			Connection cn = AccesoDB.getConnection();
+			System.out.println("Ok");
+			cn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
